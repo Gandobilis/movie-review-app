5b30a10f6acb6573314bb83eef1e8d9a6e6b1256
Gandobilis <lashadeveloper@gmail.com>
2023-10-06 12:34:50 +0400

    Login and logout routes defined and tested with postman. (works)

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,12 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::middleware('locale')->group(function () {
+    Route::post('login', [AuthController::class, 'login'])
+        ->name('auth.login');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout'])
+            ->name('auth.logout');
+    });
 });
