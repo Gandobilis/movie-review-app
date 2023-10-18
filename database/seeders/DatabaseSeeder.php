41bb32ae71b885e47569e37e4aa8d648e1906f3a
Gandobilis <lashadeveloper@gmail.com>
2023-10-18 18:41:44 +0400

    Rating Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class,
             GenreSeeder::class,
             MovieSeeder::class,
+            UserSeeder::class,
+            RatingSeeder::class,
             CollectionSeeder::class
         ]);
     }
